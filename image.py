93422b66b75225733566061eb2583e4d1a59db79
RameshRaja05 <118657318+RameshRaja05@users.noreply.github.com>
2022-11-20 16:11:35 +0530

    Delete image.py

--- image.py
+++
@@ -1,6 +0,0 @@
-
-from PIL import Image, ImageFilter
-
-img = Image.open('./images/astro.jpg')
-img.thumbnail((450,420))
-print(img.size) #it automatically fix the height and width for image it is intellisense.
